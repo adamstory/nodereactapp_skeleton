a406f78887c36c075e55d469bc3b4ba9583ac82b
adamstory <adamjstory@gmail.com>
2022-01-23 21:07:34 +0000

    Add file structure

---
+++ react-node-app/client/src/components/App/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/58025/cherry-blossom-emoji-clipart-md.png"
+          className="App-logo"
+          alt="logo"
+        />
+        <br />
+        <h2>🌸Welcome back, Adam!🌸</h2>
+        <p>React (Front-end): ✅</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
